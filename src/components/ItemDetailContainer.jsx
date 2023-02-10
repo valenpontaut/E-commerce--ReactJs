e9dcb5b687c3c6882454aa3cf5309c56de9305f0
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-02-09 22:09:41 -0300

    item details

--- src/components/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import music from '../music.json'
+import { Container } from '@chakra-ui/react'
+import ItemDetail from './ItemDetail'
 
 const ItemDetailContainer = () => {
+  const idmusic = useParams().id;
+  
+  const itemmus = music.filter( (itemmus) => itemmus.id == idmusic);
+  const item = itemmus[0]
+  
+
+  const showProducts = new Promise((resolve, reject) => {
+    if (itemmus.length > 0) {
+      setTimeout(() => {
+        resolve(itemmus);
+      }, 2000);
+    } else {
+      reject("No products were found")
+    }
+  })
+
+  showProducts
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
   return (
-    <div>ItemDetailContainer</div>
+    <Container className='body__Container'>
+      <ItemDetail item={item}/>
+    </Container>
   )
 }
 
