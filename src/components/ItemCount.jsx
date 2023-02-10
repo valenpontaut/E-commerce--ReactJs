1993a90f15f34829246f7365352d3db0d61d2c31
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-02-09 22:09:57 -0300

    item counter

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -4,40 +4,64 @@ import { Button, Flex, Center, Text } from '@chakra-ui/react';
 
 const ItemCount = ({stock}) => {
   const [amount, setAmount] = useState(0);
+  const [isDisabled, setDisabled] = useState(false);
+  
+  const [first,setFirst] = useState();
+
   const add = () => {
     setAmount(amount+1);
   }
   const substract = () => {
     setAmount(amount-1);
   }
-  if (amount >= 0) {
+  
+  const disable = () => {
+    setDisabled(true);
+  }
+  const enable = () => {
+    setDisabled(false);
+  }
+
+  if (amount <= 0) {
     return (
       <>
-      <button onClick={substract}>-</button>
-      <Text>{amount}</Text>  
-      <button onClick={add}>+</button>
+        <Flex>
+          <Center>
+            <button onClick={substract} disabled={true}>-</button>
+            <Text>{amount}</Text>  
+            <button onClick={add}>+</button>
+          </Center>
+        </Flex>
       </>
-      
+    )
+  } else if (amount === stock) {
+    return (
+      <>
+        <Flex>
+            <Center>
+              <button onClick={substract}>-</button>
+              <Text>{amount}</Text>  
+              <button onClick={add} disabled={true}>+</button>
+            </Center>
+        </Flex>
+        <Text>
+          There's no more available stock
+        </Text>
+      </>
     )
   } else {
     return (
       <>
+        <Flex>
+          <Center>
+            <button onClick={substract}>-</button>
+            <Text>{amount}</Text>  
+            <button onClick={add}>+</button>
+          </Center>
+        </Flex>
       </>
     )
   }
-          
-  return (
-    <>
-      <Flex>
-        <Center>
-         
-        </Center>
-       
-      </Flex>
-         
-    </>
-    
-  )
 }
 
 export default ItemCount
