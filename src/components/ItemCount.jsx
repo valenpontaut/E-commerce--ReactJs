e3bc7652c9c86f4b0b285c08a093c2bcb4707a93
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-02-11 11:50:52 -0300

    error pop up when amount is superior to stock

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,64 +1,79 @@
+/*The ItemCount component is a button included in the Item and ItemDetail component. It doesn't show negative values or a number superior to the available stock*/
+
 import React from 'react'
 import { useState } from 'react';
-import { Button, Flex, Center, Text } from '@chakra-ui/react';
+import { 
+  Button, 
+  Flex, 
+  Center, 
+  Text, 
+  useToast, 
+  Container 
+} from '@chakra-ui/react';
 
-const ItemCount = ({stock}) => {
+const ItemCount = ({w, stock}) => {
   const [amount, setAmount] = useState(0);
-  const [isDisabled, setDisabled] = useState(false);
-  
-  const [first,setFirst] = useState();
-
   const add = () => {
     setAmount(amount+1);
   }
   const substract = () => {
     setAmount(amount-1);
   }
-  
-  const disable = () => {
-    setDisabled(true);
-  }
-  const enable = () => {
-    setDisabled(false);
-  }
-
-  if (amount <= 0) {
+  const toast = useToast()
+    const id = 'stockId'
+    const showError = (message) => {
+      if (!toast.isActive(id)) {
+        toast({
+          id,
+          title: message,
+          position: 'bottom',
+          status: 'error',
+          isClosable: true,
+          duration: 2000
+        })
+      }
+    }
+    
+  if (amount === 0) {
     return (
       <>
-        <Flex>
-          <Center>
-            <button onClick={substract} disabled={true}>-</button>
-            <Text>{amount}</Text>  
-            <button onClick={add}>+</button>
-          </Center>
-        </Flex>
+      <Container bg='#2B3A67' color='white' rounded={'6'} w={w}>
+        <Center>
+          <Flex >
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white'>-</Button>
+            <Text py={'2'}>{amount}</Text>  
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={add}>+</Button>
+          </Flex>
+        </Center>
+      </Container>
       </>
     )
   } else if (amount === stock) {
     return (
       <>
-        <Flex>
-            <Center>
-              <button onClick={substract}>-</button>
-              <Text>{amount}</Text>  
-              <button onClick={add} disabled={true}>+</button>
-            </Center>
-        </Flex>
-        <Text>
-          There's no more available stock
-        </Text>
+      <Container bg='#2B3A67' color='white' rounded={'6'} w={w}>
+        <Center>
+          <Flex>
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={substract}>-</Button>
+            <Text py={'2'}>{amount}</Text>  
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white'  onClick={() => showError(`There's no more available stock`)}>+</Button>
+          </Flex>
+        </Center>
+      </Container>
       </>
     )
   } else {
     return (
       <>
-        <Flex>
-          <Center>
-            <button onClick={substract}>-</button>
-            <Text>{amount}</Text>  
-            <button onClick={add}>+</button>
-          </Center>
-        </Flex>
+      <Container bg='#2B3A67' color='white' rounded={'6'} w={w}>
+        <Center>
+          <Flex >
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={substract}>-</Button>
+            <Text py={'2'}>{amount}</Text>  
+            <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={add}>+</Button>
+          </Flex>
+        </Center>
+      </Container>  
       </>
     )
   }
