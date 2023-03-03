3d13b81ef37ad1152947fa7775770c583168c345
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-03 17:54:11 -0300

    adding context and routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,29 @@ import ItemDetailContainer from './components/ItemDetailContainer';
 import ItemListContainer from "./components/ItemListContainer"
 import NavBar from "./components/NavBar"
 import Cart from './components/Cart';
+import CartContextComp from './context/CartContextComp';
+import AboutUs from './components/AboutUs';
+import ContactUs from './components/ContactUs';
+import FindUs from './components/FindUs';
 
 function App() {
   return (
     <>
-    <BrowserRouter>
+    <CartContextComp>
+    <BrowserRouter> 
       <NavBar/>
       <Routes>
         <Route exact path='/' element={<ItemListContainer/>}/>
         <Route exact path='/category/:id' element={<ItemListContainer/>}/>
         <Route exact path='/item/:id' element={<ItemDetailContainer/>}/>
         <Route exact path='/cart' element={<Cart/>}/>
+        <Route exact path='/aboutus' element={<AboutUs/>}/>
+        <Route exact path='/contactus' element={<ContactUs/>}/>
+        <Route exact path='/findus' element={<FindUs/>}/>
       </Routes>
       <Footer/>
     </BrowserRouter>
+    </CartContextComp>
     </>
   )
 }
