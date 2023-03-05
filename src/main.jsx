a5b9189835f14d4fc8c3e201e2380f62a77b491b
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-04 21:44:49 -0300

    add firebase DB to main

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDlbGI5OoqEOLJaHO4Zc23qTBNOYudlrMg",
+  authDomain: "db-reactch-ecommerce.firebaseapp.com",
+  projectId: "db-reactch-ecommerce",
+  storageBucket: "db-reactch-ecommerce.appspot.com",
+  messagingSenderId: "668512895290",
+  appId: "1:668512895290:web:81f008da18e2b4d3d21003"
+};
+
+initializeApp(firebaseConfig);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <ChakraProvider>
