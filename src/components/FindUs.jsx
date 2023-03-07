4b9d6e8ae6748e0ce45d3435696fca7080e97554
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-07 18:50:21 -0300

    find us styling

--- src/components/FindUs.jsx
+++ src/components/FindUs.jsx
@@ -3,15 +3,19 @@ import React from 'react'
 const FindUs = () => {
   return (
     <div className='body__Container'>
-      Welcome to Myousic, your one-stop-shop for all things music! We offer a wide range of music products, including CDs, DVDs, and vinyl records.
-      <br/><br/>
-      You can find us online at www.myousic.com, where you can browse our extensive collection and make purchases directly on our website. We offer secure online payment options and fast shipping to ensure that you receive your products as quickly as possible.
-      <br/><br/>
-      In addition to our online store, you can also find Myousic products at select music stores and record shops throughout the country. Check our Store Locator page on our website to find a location near you.
-      <br/><br/>
-      Don't forget to follow us on social media to stay up-to-date on our latest releases, special offers, and promotions. You can find us on Facebook, Twitter, and Instagram by searching for @myousic.
-      <br/><br/>
-      Thank you for choosing Myousic as your go-to destination for all your music needs. We look forward to serving you!
+      <div className='findUs'>
+        <div className='findUs__text'>
+          Welcome to Myousic, your one-stop-shop for all things music! We offer a wide range of music products, including CDs, DVDs, books, and vinyl records.
+          <br/>
+          You can find us online at www.myousic.com, where you can browse our extensive collection and make purchases directly on our website. We offer secure online payment options and fast shipping to ensure that you receive your products as quickly as possible.
+          <br/>
+          In addition to our online store, you can also find Myousic products at select music stores and record shops throughout the country. Check our Store Locator page on our website to find a location near you.
+          <br/>
+          Don't forget to follow us on social media to stay up-to-date on our latest releases, special offers, and promotions. You can find us on Facebook, Twitter, and Instagram by searching for @myousic.
+          <br/><br/>
+          Thank you for choosing Myousic as your go-to destination for all your music needs. We look forward to serving you!
+        </div>
+      </div>
     </div>
   )
 }
