3fba5c944304b352714e94eb253f3915d55b5141
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-07 18:52:52 -0300

    appearing purchase btn

--- src/components/ItemCount.jsx
+++ src/components/ItemCount.jsx
@@ -1,12 +1,13 @@
 /*The ItemCount component is a button included in the Item and ItemDetail component. It doesn't show negative values or a number superior to the available stock*/
 
-import { Button, Flex, Center, Text, Container,useToast, Stack, Box } from '@chakra-ui/react';
+import { useToast } from '@chakra-ui/react';
 import { useState } from 'react';
-import { NavLink, Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const ItemCount = ({w, stock, onAdd}) => {
   const [counter,setCounter] = useState(1);
   const [itemStock, setItemStock] = useState(stock);
+  const [purchase, setPurchase] = useState(false)
   const toast = useToast()
   const idError = 'idError'
   const showError = (message) => {
@@ -54,46 +55,31 @@ const ItemCount = ({w, stock, onAdd}) => {
       setCounter(1);
       showSuccess(`Product succesfully added to cart`);
       onAdd(amount);
+      setPurchase(true);
     }
   }
   return (
     <>
-    <Box className='itemCount__boxBtns' pt={'1.5rem'}>
-      <Box className='itemCount__boxCounterAndAddCart'>
-        <Stack bg='#2B3A67' color='white' rounded={'6'} w={w}>
-          <Center>
-            <Flex >
-              <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={() => substract()}>-</Button>
-              <Text py={'2'}>{counter}</Text>
-              <Button _hover={{ bg: '#2B3A67' }} bg='#2B3A67' color='white' onClick={() => add()}>+</Button>
-            </Flex>
-          </Center>
-        </Stack>
-        <Button
-          _hover={{ bg: '#2B3A67' }}
-          bg='#2B3A67'
-          color='white'
-          w={'15rem'}
-          onClick={() => addToCart(counter)}>
-          <Text pr={2}>Add to cart</Text>
-          <span className="material-symbols-outlined">shopping_cart</span>
-        </Button>
-      </Box>
-      <Box pt={'1.5rem'}>
+      <div className='itemCount__btnContainer'>
+        <div className='itemCount__btnCounter'>
+          <button onClick={() => substract()}>-</button>
+          <p>{counter}</p>
+          <button onClick={() => add()}>+</button>
+        </div>
+        <div className='itemCount__btnAddCart'>
+          <button className='itemCount__btnAddCart' onClick={() => addToCart(counter)}>
+            <p>Add to cart</p>
+            <span className="material-symbols-outlined">shopping_cart</span>
+          </button>
+        </div>
+      </div>
+      {purchase ?
         <Link to={'/cart'}>
-          <Button _hover={{ bg: '#496A81' }}
-              bg='#66999B'
-              color='white'
-              w={'35rem'}
-              >
-              Finish my purchase
-            </Button>
+          <button className='itemCount__btnPurchase'>
+              Purchase
+          </button>
         </Link>
-      </Box>
-    </Box>
-      
-      
-
+      : null}
     </>
   )
 } 
