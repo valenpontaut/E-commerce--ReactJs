53b51d5bb9d4adf571e7c1c027e8fbe0642f4b86
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-07 18:58:35 -0300

    hotfix item list

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -5,43 +5,33 @@ import Item from './Item'
 import { Flex, Center, Text, Image } from '@chakra-ui/react'
 
 const ItemList = ({products}) => {
-  console.log('itemlist',products)
   return(
     <>
-
-
-
-
-{/*       
-    {products ?
-      
+    {(products.length === 0) ?
+      <>
+      <Center><Image src='/src/img/oops.jpg'></Image></Center>
+      <Center><Text className='item__oopsText'>Unfortunatly we don't have any products with this format today</Text></Center>
+      </>
+      :
       <Center>        
         <Flex>
           {products?.map((prod)=>(
             <Item
-              key={prod.id}
-              id={prod.id}
-              name={prod.name}
-              artist={prod.artist}
-              description={prod.description}
-              price={prod.price}
-              stock={prod.stock}
-              category={prod.category}
-              img={prod.img}
-            />
+            key={prod.id}
+            id={prod.id}
+            name={prod.name}
+            artist={prod.artist}
+            description={prod.description}
+            price={prod.price}
+            stock={prod.stock}
+            category={prod.category}
+            img={prod.img}/>
           ))}
         </Flex>            
       </Center>
-    : 
-      <>
-      <Center><Image src='/src/img/oops.jpg'></Image></Center>
-      <Center><Text className='item__oopsText'>Unfortunatly we don't have any products with this format today</Text></Center>
-      </>} */}
+    }
     </>
-    
-  
-  )
-  
+  ) 
 }
 
 export default ItemList
