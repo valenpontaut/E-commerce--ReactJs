8bac9bb492b3f3e39f911f3a60f4293f68bb03ef
Valentina Llanos Pontaut <105258399+valenpontaut@users.noreply.github.com>
2023-03-07 18:50:00 -0300

    about us styling

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -2,18 +2,24 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <div className='body__Container'> Welcome to Myousic, your go-to destination for all things music!
-    At Myousic, we are passionate about music and understand the importance of having a physical collection of your favorite artists and albums. We believe that music is not just something you listen to, but a way of life.
-    <br/><br/>
-    Our e-commerce project specializes in providing music lovers with an extensive selection of CDs, DVDs, and vinyl records from all genres, including rock, pop, jazz, classical, hip-hop, and more. Our team is dedicated to curating the best selection of music products to cater to all tastes and preferences.
-    <br/><br/>
-    Our mission at Myousic is to bring back the joy of owning physical music products and reviving the nostalgia of visiting your favorite record store. We strive to provide our customers with the best shopping experience, with a user-friendly website that is easy to navigate and browse through our collection.
-    <br/><br/>
-    Our commitment to quality is reflected in the products we offer. We work with reputable suppliers and ensure that all our products are authentic and of the highest quality.
-    <br/><br/>
-    We are more than just an e-commerce project; we are a community of music enthusiasts who share a passion for music. Our goal is to connect with our customers, build lasting relationships, and create a platform where music lovers can come together to share their love for music.
-    <br/><br/>
-    Thank you for choosing Myousic as your go-to destination for all things music. We hope you enjoy our products as much as we enjoy curating them for you! </div>
+    <div className='body__Container'>
+      <div className='aboutUs'>
+        <div className='aboutUs__text'>
+          Welcome to Myousic, your go-to destination for all things music!
+          At Myousic, we are passionate about music and understand the importance of having a physical collection of your favorite artists and albums. We believe that music is not just something you listen to, but a way of life.
+          <br/><br/>
+          Our e-commerce project specializes in providing music lovers with an extensive selection of CDs, DVDs, and vinyl records from all genres, including rock, pop, jazz, classical, hip-hop, and more. Our team is dedicated to curating the best selection of music products to cater to all tastes and preferences.
+          <br/><br/>
+          Our mission at Myousic is to bring back the joy of owning physical music products and reviving the nostalgia of visiting your favorite record store. We strive to provide our customers with the best shopping experience, with a user-friendly website that is easy to navigate and browse through our collection.
+          <br/><br/>
+          Our commitment to quality is reflected in the products we offer. We work with reputable suppliers and ensure that all our products are authentic and of the highest quality.
+          <br/><br/>
+          We are more than just an e-commerce project; we are a community of music enthusiasts who share a passion for music. Our goal is to connect with our customers, build lasting relationships, and create a platform where music lovers can come together to share their love for music.
+          <br/><br/>
+          Thank you for choosing Myousic as your go-to destination for all things music. We hope you enjoy our products as much as we enjoy curating them for you! 
+        </div>
+      </div>
+    </div>
   )
 }
 
